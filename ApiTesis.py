99daba28c8ba224c5a065372fef55c62c6601715
Santiago Moreno <santiago.moreno.dev01@gmail.com>
2025-01-30 09:23:20 -0500

    Primer commit

---
+++ ApiTesis.py
@@ -0,0 +1,67 @@
+from tensorflow import keras
+import pandas as pd
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import io
+
+app = Flask(__name__)
+CORS(app)
+
+# Cargar el modelo guardado
+model = keras.models.load_model('modelo_precision_acumulado.h5')
+model.summary()
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    # Verificar si el archivo fue enviado
+    if 'file' not in request.files:
+        return jsonify({'error': 'No se envió un archivo'}), 400
+
+    file = request.files['file']
+
+   
+    try:
+        # Leer el archivo Excel usando pandas
+        df = pd.read_excel(file)
+    
+        # Preprocesar los datos (asegúrate de que solo contengan columnas numéricas)
+        # Reemplaza 'columnas_relevantes' con las columnas que tu modelo necesita
+        columnas_relevantes = ['Variedad_B', ' mm Precipitation', ' m/s Gust Speed', 'IP', ' mm/h Max Precip Rate', 'Semana', 'Variedad_W', 'Variedad_V',' °C Air Temperature', 'PFIG','PFG','Variedad_L']  # Ejemplo de columnas necesarias
+        
+        missing_columns = [col for col in columnas_relevantes if col not in df.columns]
+        if missing_columns:
+            return jsonify({'error': f"Faltan las siguientes columnas en el archivo: {missing_columns}"}), 400
+
+    
+        # Seleccionar solo las columnas necesarias
+        input_data = df[columnas_relevantes]
+    
+        # Manejar valores faltantes (opcional: rellena con 0 o la media)
+        input_data = input_data.fillna(0)
+    
+        # Verifica que todas las columnas sean numéricas
+        input_data = input_data.astype(float)
+    
+        # Hacer predicciones con el modelo
+        predictions = model.predict(input_data)
+    
+        # Agregar las predicciones como una nueva columna en el DataFrame
+        df['Predicciones'] = predictions.tolist()
+    
+        # Crear un archivo Excel en memoria con las predicciones añadidas
+        output = io.BytesIO()
+       # df.to_excel('mi_archivo_con_predicciones3.xlsx', index=False)
+        df.to_excel(output, index=False)
+        
+        output.seek(0)  # Volver al principio del archivo en memoria
+    
+        # Devolver el archivo Excel con las predicciones al cliente
+        return send_file(output, as_attachment=True, download_name="predicciones_con_resultados.xlsx", mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
